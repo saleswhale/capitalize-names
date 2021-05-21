499b9b6280553da154f4a9ffb3e38766f73a7796
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-05-21 17:36:38 +0800

    Remove whitespaces

--- lib/capitalize_names/capitalizer.rb
+++ lib/capitalize_names/capitalizer.rb
@@ -5,7 +5,6 @@ module CapitalizeNames
     HYPHEN = /(\s*-\s*)/
 
     MC = /^Mc(\w)(?=\w)/i
-    MAC = /^Mac(\w)(?=\w)/i
     O_APOSTROPHE = /^O'(\w)(?=\w)/i
 
     def initialize(name)
@@ -66,8 +65,8 @@ module CapitalizeNames
           hyphens << [_start, _end, _value]
         end
 
-        _name = _name.split.map{ |w| 
-          w.mb_chars.capitalize.to_str 
+        _name = _name.split.map{ |w|
+          w.mb_chars.capitalize.to_str
         }.map{ |w|
           w.gsub(MC) { "Mc#{$1.upcase}" }\
            .gsub(O_APOSTROPHE) { "O'#{$1.upcase}" }
@@ -82,7 +81,7 @@ module CapitalizeNames
             _name = _name[0..._start] << _value << (_name[_start+1..-1] || "")
           end
         end
-            
+
         _name = _name.gsub("Van ", "van ").gsub("De ", "de ").gsub("Dit ", "dit ")
         _name << " "
 
